e278cb0086ad2a8da032a191f50ca3e46b2797e7
taxnfin <kvillafuerte@taxnfin.com>
2026-05-08 17:45:53 -0600

    Add contalink_financial router to API

--- backend/server.py
+++ backend/server.py
@@ -51,6 +51,7 @@ from routes.bank_import import router as bank_import_router
 from routes.notifications import router as notifications_router
 from routes.integrations import router as integrations_router
 from routes.contalink import router as contalink_router
+from routes.contalink_financial import router as contalink_financial_router
 from routes.account_mappings import router as account_mappings_router
 
 # ===== REGISTER ROUTERS =====
@@ -85,6 +86,7 @@ api_router.include_router(bank_import_router)
 api_router.include_router(notifications_router)
 api_router.include_router(integrations_router)
 api_router.include_router(contalink_router)
+api_router.include_router(contalink_financial_router)
 api_router.include_router(account_mappings_router)
 
 # ===== MIDDLEWARE =====
